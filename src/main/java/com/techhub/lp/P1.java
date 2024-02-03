f35324d9361ef4571d6d47e1b196e49fbd7403c3
Ram Niwash <ram_niwash@outlook.com>
2024-02-03 07:54:30 +0530

    Init commit

---
+++ src/main/java/com/techhub/lp/P1.java
@@ -0,0 +1,29 @@
+package com.techhub.lp;
+
+/**
+  
+ Program #1 : Write a program to drew the following pattern.
+  
+ ********
+ ********
+ ********
+ ********
+ ********
+   
+ */
+public class P1 {
+	
+	public static void main(String[] args) {
+		draw(5);
+	}
+	
+	private static void draw(int number) {
+		
+		for(int i=1;i<=number;i++) {
+			for(int j=0;j<=number;j++) {
+				System.out.print("*"); 
+			}
+			System.out.println();
+		}
+	}
+}
