c14adcb258f1c5daa7d290e865a3f2ee6f71faad
ramniwash <ram.niwash.m@outlook.com>
2024-03-20 16:07:15 +0530

    Add logical program P58

--- src/main/java/com/techhub/Test.java
+++ src/main/java/com/techhub/Test.java
@@ -1,25 +1,8 @@
 package com.techhub;
 
-import com.techhub.collections.Employee;
-import com.techhub.collections.Employees;
-
-import java.io.DataInputStream;
-import java.io.InputStream;
-import java.time.Year;
-import java.util.*;
-import java.util.stream.Stream;
-
 public class Test {
 
     public static void main(String[] args) {
 
-        Integer[][] intArr = {{1,2}, {3,4}, {5,6}, {7,8}};
-        Stream<Integer[]> myStream = Stream.of(intArr);
-
-        Stream<Integer> stream = myStream.flatMap(arr->{
-            return Stream.of(arr);
-        });
-
-        stream.forEach(System.out::println);
     }
 }
---
+++ src/main/java/com/techhub/lp/P58.java
@@ -0,0 +1,59 @@
+package com.techhub.lp;
+
+import java.util.Scanner;
+
+/**
+ ==================================================================================================
+ Program #58 : Write a program to get the Ordinal number as following.
+ --------------------------------------------------------------------------------------------------
+
+ 1 = 1st
+ 2 = 2nd
+ 3 = 3rd
+ 7 = 7th
+ 51 = 51st
+
+ ==================================================================================================
+*/
+public class P58 {
+
+	private static final String[] ORDINALS = {"th","st","nd","rd"};
+
+	public static void main(String[] args) throws Exception {
+		System.out.println("1 =" + getOrdinal(1));
+		System.out.println("2 =" + getOrdinal(2));
+		System.out.println("3 =" + getOrdinal(3));
+		System.out.println("7 =" + getOrdinal(7));
+		System.out.println("11 =" + getOrdinal(11));
+		System.out.println("12 =" + getOrdinal(12));
+		System.out.println("13 =" + getOrdinal(13));
+		System.out.println("19 =" + getOrdinal(19));
+		System.out.println("51 =" + getOrdinal(51));
+		System.out.println("52 =" + getOrdinal(52));
+		System.out.println("53 =" + getOrdinal(53));
+		System.out.println("57 =" + getOrdinal(57));
+		System.out.println("71 =" + getOrdinal(71));
+		System.out.println("72 =" + getOrdinal(72));
+		System.out.println("73 =" + getOrdinal(73));
+		System.out.println("74 =" + getOrdinal(74));
+		System.out.println("97 =" + getOrdinal(97));
+	}
+
+	private static String getOrdinal(int number){
+
+		String ordinal;
+		if(number>=0 && number<=3){
+			ordinal = number+ORDINALS[number];
+		}else if(number>3 && number<21){
+			ordinal = number+ORDINALS[0];
+		}else{
+			int r = number%10;
+			if(r<=3){
+				ordinal = number+ORDINALS[r];
+			}else{
+				ordinal = number+ORDINALS[0];
+			}
+		}
+		return ordinal;
+	}
+}
